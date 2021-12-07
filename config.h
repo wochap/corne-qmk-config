c12e1cb6e6878d9af7204e92795356d00bbe01fc
wochap <gean.marroquin@gmail.com>
2021-12-06 21:20:45 -0500

    style

--- config.h
+++ config.h
@@ -1,21 +1,20 @@
 #pragma once
 
-//#define USE_MATRIX_I2C
+// #define USE_MATRIX_I2C
 
 /* Select hand configuration */
-
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-// #define SSD1306OLED
 #undef USE_I2C
 #undef SSD1306OLED
 
 #define USE_SERIAL_PD2
 
 // #define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 300
+#undef TAPPING_TERM
+#define TAPPING_TERM 170
 #define IGNORE_MOD_TAP_INTERRUPT
 
 #define OLED_FONT_H "keyboards/crkbd/keymaps/soundmonster/glcdfont.c"
